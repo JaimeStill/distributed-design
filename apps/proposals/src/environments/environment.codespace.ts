f1ac07d170ccccdd356672a49583c7ff63fad46c
Jaime Still <jpstill85@gmail.com>
2023-09-20 15:08:32 +0000

    CORS configuration

--- apps/proposals/src/environments/environment.codespace.ts
+++ apps/proposals/src/environments/environment.codespace.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    server: 'https://fuzzy-rotary-phone-5gx47779gr5c4g66-5001.app.github.dev/',
-    api: 'https://fuzzy-rotary-phone-5gx47779gr5c4g66-5001.app.github.dev/api/',
-    events: 'https://fuzzy-rotary-phone-5gx47779gr5c4g66-5001.app.github.dev/events/'
+    server: 'https://curly-cod-97jqgggxjgqf97x9-5001.app.github.dev/',
+    api: 'https://curly-cod-97jqgggxjgqf97x9-5001.app.github.dev/api/',
+    events: 'https://curly-cod-97jqgggxjgqf97x9-5001.app.github.dev/events/'
 }
