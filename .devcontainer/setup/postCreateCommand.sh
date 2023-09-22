bac696bb4827f5191cae5d8c8e6e59bbbe666d3c
Jaime Still <jpstill85@gmail.com>
2023-09-22 12:45:16 -0400

    .devcontainer: configure codespace endpoints

--- .devcontainer/setup/postCreateCommand.sh
+++ .devcontainer/setup/postCreateCommand.sh
@@ -12,10 +12,10 @@ gh codespace ports visibility 5002:public -c $CODESPACE_NAME
 # configure codespace endpoints
 replace="s/(?<=https:\/\/)(.+)(?=\-(\d+)\.app\.github\.dev)/$CODESPACE_NAME/g"
 proposals="./apps/proposals/src/environments/environment.codespace.ts"
-workspaces="./apps/workspaces/src/environments/environment.codespace.ts"
+workflows="./apps/workflows/src/environments/environment.codespace.ts"
 
 perl -pe $replace -i $proposals
-perl -pe $replace -i $workspaces
+perl -pe $replace -i $workflows
 
 # install dotnet tools
 dotnet tool install -g dotnet-ef
