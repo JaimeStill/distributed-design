bfc81090c1a3cfa90bbdda23e3a9efec674ea39b
Jaime Still <jpstill85@gmail.com>
2023-09-22 12:46:03 -0400

    remove test files

--- modified.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    server: 'https://some-gnarly-toast-32oifn293f-5001.app.github.dev/',
-    api: 'https://some-gnarly-toast-32oifn293f-5001.app.github.dev/api/',
-    events: 'https://some-gnarly-toast-32oifn293f-5001.app.github.dev/events/'
-}
--- testsettings.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    server: 'https://supreme-space-barnacle-pjpg5556p95c6wgw-5001.app.github.dev/',
-    api: 'https://supreme-space-barnacle-pjpg5556p95c6wgw-5001.app.github.dev/api/',
-    events: 'https://supreme-space-barnacle-pjpg5556p95c6wgw-5001.app.github.dev/events/'
-}
