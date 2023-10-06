a25b78e66b9dec77d0417654f2e7c1c907bca760
Jaime Still <jpstill85@gmail.com>
2023-10-06 13:41:12 -0400

    optimize app tasks

---
+++ scripts/clean-apps.ps1
@@ -0,0 +1,31 @@
+function Remove-Artifact([string] $path) {
+    if (Test-Path $path) {
+        Remove-Item $path -Force -Recurse
+    }
+}
+
+function Remove-Artifacts([string] $path) {
+    Write-Host "Removing artifacts for $path" -ForegroundColor Blue
+
+    $dist = Join-Path $path 'dist'
+    $modules = Join-Path $path 'node_modules'
+    $angular = Join-Path $path '.angular'
+
+    Remove-Artifact $dist
+    Remove-Artifact $modules
+    Remove-Artifact $angular
+
+    Write-Host "Artifacts successfully removed" -ForegroundColor Green
+}
+
+$Directories = @(
+    "../apps/libs/core/",
+    "../apps/libs/contracts/core/",
+    "../apps/libs/contracts/workflows/",
+    "../apps/proposals/",
+    "../apps/workflows/"
+)
+
+foreach ($directory in $Directories) {
+    Remove-Artifacts $directory
+}
