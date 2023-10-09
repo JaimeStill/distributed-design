b656f98467163dbad2e53519f17155d227af9dc6
Jaime Still <jpstill85@gmail.com>
2023-10-09 19:42:45 +0000

    tasks: fix clean-apps

--- apps/workflows/src/environments/environment.codespace.ts
+++ apps/workflows/src/environments/environment.codespace.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    server: 'https://organic-rotary-phone-q7p9555x9q5fx5vg-5002.app.github.dev/',
-    api: 'https://organic-rotary-phone-q7p9555x9q5fx5vg-5002.app.github.dev/api/',
-    events: 'https://organic-rotary-phone-q7p9555x9q5fx5vg-5002.app.github.dev/events/',
-    workflowsGateway: 'https://organic-rotary-phone-q7p9555x9q5fx5vg-5002.app.github.dev/gateway/'
+    server: 'https://glorious-giggle-r4pr555xrw4fpw4j-5002.app.github.dev/',
+    api: 'https://glorious-giggle-r4pr555xrw4fpw4j-5002.app.github.dev/api/',
+    events: 'https://glorious-giggle-r4pr555xrw4fpw4j-5002.app.github.dev/events/',
+    workflowsGateway: 'https://glorious-giggle-r4pr555xrw4fpw4j-5002.app.github.dev/gateway/'
 }
